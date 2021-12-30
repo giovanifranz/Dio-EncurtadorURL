454955ffe2cbc68d93678bac3429d98ca8b44bed
Giovani Franz <giovanifranz151@gmail.com>
2021-12-30 00:17:39 -0300

    Delete dist directory

--- dist/config/constant.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-exports.config = {
-    API_URL: "http://localhost:3333",
-    MONGO_CONNECTION: 'mongodb://localhost:27017/database'
-};
-//# sourceMappingURL=constant.js.map
